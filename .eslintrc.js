a978a085fedc95220f9a2054fa4d83ea1d217323
Alexey197 <margadonnn3@gmail.com>
2021-01-15 15:14:32 +0300

    eslint commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+      "es6": true,
+      "browser": true,
+      "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+      "semi": "off",
+      "arrow-parens": "off",
+      "comma-dangle": "off",
+      "require-jsdoc": "off",
+      "no-trailing-spaces": "off",
+      "operator-linebreak": "off"
+    }
+};
